8518d5243fd52078e90cb7423466cdffd757f201
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2025-04-10 23:47:42 -0300

    Mejoradas tarjetas de peliculas

--- the_vanilla_project/app.js
+++ the_vanilla_project/app.js
@@ -1,4 +1,4 @@
-let paginaActual = 90;
+let paginaActual = 1;
 let tamanioPagina = 10;
 let totalPeliculas = 0;
 let totalPaginas = 0;
@@ -8,6 +8,9 @@ let endpointPaginaSiguiente = null;
 const MOVIE_API_URL = 'https://the-movie-api-ykho.onrender.com';
 const endpointBackend = `${MOVIE_API_URL}/movies?page=${paginaActual}&size=${tamanioPagina}`;
 
+const MOVIE_DB_URL = 'https://api.themoviedb.org/3/search/movie?api_key=da1321128843457e4bab639b36d150e4'
+const MOVIE_POSTERS_DB_URL = 'https://image.tmdb.org/t/p/w500'
+
 const contadoresPaginaActual = document.querySelectorAll('.pagina-actual');
 const botonesPaginaAnterior = document.querySelectorAll('.pagina-anterior');
 const botonesPaginaSiguiente = document.querySelectorAll('.pagina-siguiente');
@@ -16,7 +19,6 @@ const botonReset = document.querySelector('.reset');
 botonReset.addEventListener('click', function () {
     const main = document.querySelector('main');
     main.innerHTML = '';
-    console.log(main);
 });
 
 for (boton of botonesPaginaAnterior) {
@@ -50,11 +52,26 @@ function actualizarDatosEnUi(respuestaEnTexto) {
     }
     
     const main = document.querySelector('main');
+    main.innerHTML = '';
 
     for (const pelicula of peliculas) {
         const tarjetaPelicula = crearTarjetaPelicula(pelicula);
         main.appendChild(tarjetaPelicula);
+        obtenerUrlPosterPelicula(pelicula.title, pelicula.year).then((urlPoster) => {
+            tarjetaPelicula.style.backgroundImage = `url(${MOVIE_POSTERS_DB_URL + urlPoster})`;
+            tarjetaPelicula.children[0].style.display = 'none';
+        });
     }
+}
+
+async function obtenerUrlPosterPelicula(tituloPelicula, anioPelicula) {
+    const resultado = tituloPelicula.split(" ").join("+");
+    const endpointMovieDb = `${MOVIE_DB_URL}&query=${resultado}&year=${anioPelicula}`;
+    const respuesta = await fetch(endpointMovieDb);
+    if (!respuesta.ok) {
+        throw new Error(`HTTP error: ${respuesta.status}`);
+    }
+    return JSON.parse(await respuesta.text())['results'][0]['poster_path'];
 }
 
 function crearTarjetaPelicula(pelicula) {
